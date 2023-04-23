893df9cddc3c4c536d351a38da38b61f6b5bd8bc
eunsookim1 <92eunsoo@gmail.com>
2023-04-24 01:18:20 +0900

    output tests

---
+++ index.js
@@ -0,0 +1,76 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+ 
+// // fetchMyIP((error, ip) => {
+// //   if (error) {
+// //     console.log("It didn't work!" , error);
+// //     return;
+// //   }
+
+// //   console.log('It worked! Returned IP:' , ip);
+// // });
+
+// // fetchCoordsByIP('220.89.40.226', (error, coordinates) => {
+// //   if (error) {
+// //     console.log("It didn't work!, error");
+// //     return;
+// //   }
+
+// //   console.log('It worked! Returned coordinates:', coordinates);
+// // });
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flover times and durations:', passTimes);
+// });
+
+
+/**
+ * Input:
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns:
+ *   undefined
+ * Sideffect:
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
+
+
+
+
+
+//220.89.40.226
+//58.125.239.119
+
+// Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+// Next pass at Fri Jun 01 2021 14:36:08 GMT-0700 (Pacific Daylight Time) for 632 seconds!
+// Next pass at Fri Jun 01 2021 16:12:35 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+// Next pass at Fri Jun 01 2021 17:49:29 GMT-0700 (Pacific Daylight Time) for 648 seconds!
+// Next pass at Fri Jun 01 2021 19:26:12 GMT-0700 (Pacific Daylight Time) for 643 seconds!

cf59b13a68d1fba16c7ff236bd1dc4268ae17f37
eunsookim1 <92eunsoo@gmail.com>
2023-04-24 11:17:10 +0900

    index using promise

---
+++ index2.js
@@ -0,0 +1,22 @@
+const { nextISSTImesForMyLocation } = require('./iss_promised');
+
+// converting the timeStamp into a readable date and time for my timezone
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// promise instead of callback waterfall
+nextISSTImesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+  
